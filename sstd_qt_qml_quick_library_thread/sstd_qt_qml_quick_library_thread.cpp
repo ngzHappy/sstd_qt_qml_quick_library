1068a3231a75dfd87004f8402a5302be8e99aa27
ngzHappy <zhaixueqiang@hotmail.com>
2019-03-17 22:36:58 +0800

    +unchecked

--- sstd_qt_qml_quick_library_thread/sstd_qt_qml_quick_library_thread.cpp
+++ sstd_qt_qml_quick_library_thread/sstd_qt_qml_quick_library_thread.cpp
@@ -2,6 +2,9 @@
 #include <mutex>
 #include <shared_mutex>
 
+#include <boost/context/fiber.hpp>
+#include <boost/context/protected_fixedsize_stack.hpp>
+
 namespace _theSSTDLibraryMemoryFile {
 
     using Mutex = std::shared_mutex;
@@ -12,19 +15,68 @@ namespace _theSSTDLibraryMemoryFile {
 
 namespace sstd {
 
-    ThreadObject::ThreadObject(){
+    class ThreadYieldObjectPrivate {
+    public:
+        using fiber_t = boost::context::fiber;
+        std::optional< fiber_t > fiber;
+        fiber_t * functionFiber{ nullptr };
+    private:
+        sstd_class(ThreadYieldObjectPrivate);
+    };
+
+    ThreadYieldObject::ThreadYieldObject(std::size_t argStackSize) :
+        thisPrivate(sstd_new<ThreadYieldObjectPrivate>()) {
+        using fiber_t = ThreadYieldObjectPrivate::fiber_t;
+        thisPrivate->fiber.emplace(
+            std::allocator_arg,
+            boost::context::protected_fixedsize_stack{ argStackSize },
+            [this](fiber_t && f)->fiber_t {
+            thisPrivate->functionFiber = &f;
+            f = std::move(f).resume();
+            this->run();
+            return std::move(f);
+        });
+        (*(thisPrivate->fiber)) = std::move(*(thisPrivate->fiber)).resume();
     }
 
-    ThreadObject::~ThreadObject(){
+    ThreadYieldObject::~ThreadYieldObject() {
+        delete thisPrivate;
     }
 
-    SSTD_QT_SYMBOL_DECL ThreadObject * getThreadObject(QThread * arg){
+    void ThreadYieldObject::yield(QObject * arg) {
+        if (arg == nullptr) {
+            return;
+        }
+        QMetaObject::invokeMethod(arg, [varThis = this->shared_from_this()]() {
+            auto varPrivate = varThis->thisPrivate;
+            (*(varPrivate->fiber)) = std::move(*(varPrivate->fiber)).resume();
+        }, Qt::QueuedConnection);
+    }
 
-        if(arg==nullptr){
+    void ThreadYieldObject::run() noexcept {
+        sstd_try{
+            this->doRun();
+        }sstd_catch(...) {
+            sstd_on_exception();
+        }
+    }
+
+    void ThreadYieldObject::doRun() {
+    }
+
+    ThreadObject::ThreadObject() {
+    }
+
+    ThreadObject::~ThreadObject() {
+    }
+
+    SSTD_QT_SYMBOL_DECL ThreadObject * getThreadObject(QThread * arg) {
+
+        if (arg == nullptr) {
             return nullptr;
         }
 
-        if( arg->isFinished() ){
+        if (arg->isFinished()) {
             return nullptr;
         }
 
@@ -36,8 +88,8 @@ namespace sstd {
         {
             std::shared_lock varReadLock{
                 _theSSTDLibraryMemoryFile::getThreadObjectMutex(arg) };
-            varAns = arg->userData( _theSSTDLibraryMemoryFile::threadObjectID()  );
-            if(varAns){
+            varAns = arg->userData(_theSSTDLibraryMemoryFile::threadObjectID());
+            if (varAns) {
                 return static_cast<ThreadObject *>(varAns);
             }
         }
@@ -45,13 +97,13 @@ namespace sstd {
         {
             std::unique_lock varWriteLock{
                 _theSSTDLibraryMemoryFile::getThreadObjectMutex(arg) };
-            varAns = arg->userData( _theSSTDLibraryMemoryFile::threadObjectID()  );
-            if(varAns){
+            varAns = arg->userData(_theSSTDLibraryMemoryFile::threadObjectID());
+            if (varAns) {
                 return static_cast<ThreadObject *>(varAns);
             }
             varAnsObject = sstd_new<ThreadObject>();
             varAnsObject->moveToThread(arg);
-            arg->setUserData( _theSSTDLibraryMemoryFile::threadObjectID() , varAnsObject );
+            arg->setUserData(_theSSTDLibraryMemoryFile::threadObjectID(), varAnsObject);
         }
 
         return varAnsObject;
@@ -62,26 +114,26 @@ namespace sstd {
 
 namespace _theSSTDLibraryMemoryFile {
 
-    inline uint threadObjectID(){
+    inline uint threadObjectID() {
         static const uint varAns =
-                QObject::registerUserData();
+            QObject::registerUserData();
         return varAns;
     }
 
-    template<typename mutex,std::size_t N,std::size_t M >
-    inline mutex & getThreadObjectMutex(QThread * arg){
-        using mutex_array_type = std::array< mutex , 1<<N >;
+    template<typename mutex, std::size_t N, std::size_t M >
+    inline mutex & getThreadObjectMutex(QThread * arg) {
+        using mutex_array_type = std::array< mutex, 1 << N >;
         static mutex_array_type * varMutex =/*never delete*/
-                sstd_new<mutex_array_type>();
+            sstd_new<mutex_array_type>();
         auto var =
             reinterpret_cast<std::uintptr_t>(arg);
         var >>= M;
-        var &= ((1<<N)-1);
+        var &= ((1 << N) - 1);
         return (*varMutex)[var];
     }
 
-    inline Mutex & getThreadObjectMutex(QThread *arg){
-        return getThreadObjectMutex<Mutex,7,2>(arg);
+    inline Mutex & getThreadObjectMutex(QThread *arg) {
+        return getThreadObjectMutex<Mutex, 7, 2>(arg);
     }
 
 }/*namespace _theSSTDLibraryMemoryFile*/
--- sstd_qt_qml_quick_library_thread/sstd_qt_qml_quick_library_thread.hpp
+++ sstd_qt_qml_quick_library_thread/sstd_qt_qml_quick_library_thread.hpp
@@ -23,6 +23,25 @@ namespace sstd {
 
     SSTD_QT_SYMBOL_DECL ThreadObject * getThreadObject(QThread *);
 
+    class ThreadYieldObjectPrivate;
+    class SSTD_QT_SYMBOL_DECL ThreadYieldObject : 
+        public std::enable_shared_from_this< ThreadYieldObject > {
+        sstd_delete_copy_create(ThreadYieldObject);
+    private:
+        ThreadYieldObjectPrivate * const thisPrivate;
+    public:
+        void run() noexcept ;
+        virtual ~ThreadYieldObject();
+        ThreadYieldObject(std::size_t=1024uLL*1024uLL*64);
+    public:
+        void yield( QObject * );
+    protected:
+        virtual void doRun() = 0;
+    private:
+        sstd_class(ThreadYieldObject);
+    };
+
+
 }/*namespace sstd*/
 
 
