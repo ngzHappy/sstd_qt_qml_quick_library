9ba82d7c4c3eeb8ff07f5749dd8a29fc7fc40cbb
ngzHappy <zhaixueqiang@hotmail.com>
2019-05-15 18:29:05 +0800

    +update

--- sstd_qt_qml_quick_library_application/private_application.cpp
+++ sstd_qt_qml_quick_library_application/private_application.cpp
@@ -8,9 +8,11 @@
 namespace sstd {
 
     namespace global {
+
         inline QString fallBackStyle() {
             return QStringLiteral("Material");
         }
+
 #if defined(_DEBUG)
         bool isQtStyledApplication{ false };
 #endif
@@ -28,6 +30,7 @@ namespace sstd {
     namespace global {
 
         StaticGlobal::StaticGlobal() {
+            QQmlEngine::setObjectOwnership(this, QQmlEngine::CppOwnership);
         }
 
         StaticGlobal::~StaticGlobal() {
@@ -39,12 +42,85 @@ namespace sstd {
             return 1;
         }
 
-        Static::Static(StaticGlobal * a, QObject * b) :
-            thisSharedObject{ a },
-            thisAppStyle{ b } {
+        /*data copy from : qquickmaterialstyle.cpp */
+        inline int globalGetElevation() {
+            return 0;
         }
 
-        Static::~Static() {
+        namespace dark {
+
+            inline QColor globalGetAccent() {
+                return QColor(0x0f4, 0x08f, 0x0b1);
+            }
+
+            inline QColor globalGetBackground() {
+                return QColor(0x030, 0x030, 0x030);
+            }
+
+            inline QColor globalGetForeground() {
+                return QColor(0x0ff, 0x0ff, 0x0ff);
+            }
+
+            inline QColor globalGetPrimary() {
+                return QColor(0x09f, 0x0a8, 0x0da);
+            }
+
+        }/*namespace drak*/
+
+        namespace light {
+
+            inline QColor globalGetAccent() {
+                return QColor(0x0e9, 0x01e, 0x063);
+            }
+
+            inline QColor globalGetBackground() {
+                return QColor(0x0fa, 0x0fa, 0x0fa);
+            }
+
+            inline QColor globalGetForeground() {
+                return QColor(0x000, 0x000, 0x000);
+            }
+
+            inline QColor globalGetPrimary() {
+                return QColor(0x03f, 0x051, 0x0b5);
+            }
+
+        }/*namespace light*/
+
+        int StaticGlobal::getElevation() const {
+            return globalGetElevation();
+        }
+
+        QColor StaticGlobal::getAccent() const {
+            return isDark() ? dark::globalGetAccent() : light::globalGetAccent();
+        }
+
+        QColor StaticGlobal::getBackground() const {
+            return isDark() ? dark::globalGetBackground() : light::globalGetBackground();
+        }
+
+        QColor StaticGlobal::getForeground() const {
+            return isDark() ? dark::globalGetForeground() : light::globalGetForeground();
+        }
+
+        QColor StaticGlobal::getPrimary() const {
+            return isDark() ? dark::globalGetPrimary() : light::globalGetPrimary();
+        }
+
+        bool StaticGlobal::isDark() const {
+            return thisIsDark;
+        }
+
+        void StaticGlobal::setIsDark( bool arg) {
+            if (arg == thisIsDark) {
+                return;
+            }
+            thisIsDark = arg;
+            accentChanged();
+            backgroundChanged();
+            foregroundChanged();
+            primaryChanged();
+            isDarkChanged();
         }
 
         inline auto instanceStaticGlobal() {
@@ -58,41 +134,13 @@ namespace sstd {
 inline static void registerThis() {
     using namespace sstd::global;
     const char * globalURI = "sstd.styled.app";
-    qmlRegisterUncreatableType< StaticGlobal >(globalURI, 1, 0,
-        "StaticGlobal","can not create StaticGlobal");
-    qmlRegisterSingletonType< Static >(globalURI, 1, 0, "GlobalAppData",
-        [](QQmlEngine *engine, QJSEngine *)->QObject * {
 
+    qmlRegisterSingletonType< StaticGlobal >(globalURI, 1, 0, "GlobalAppData",
+        [](QQmlEngine *, QJSEngine *)->QObject * {
 #if defined(_DEBUG)
-        assert(engine);
-        assert(sstd::global::isQtStyledApplication);
+        assert(isQtStyledApplication);
 #endif
-
-        QQmlComponent varComponent{ engine };
-        varComponent.setData(QByteArrayLiteral(u8R"(
-
-import QtQuick 2.12
-import QtQuick.Controls 2.12
-import QtQuick.Controls.Material 2.12
-
-Pane {
-    // property var xxx : ApplicationWindow.window.Material ;
-    function getStyle() {
- 
-        return ApplicationWindow.window.appStyle ;
-    }
-}
-
-)"), QUrl{});
-
-        auto varContexObject = varComponent.beginCreate(engine->rootContext());
-        varComponent.completeCreate();
-
-        auto varAns = sstd_new<Static>(instanceStaticGlobal(), varContexObject);
-        varContexObject->setParent(varAns);
-
-        return varAns;
-
+        return instanceStaticGlobal();
     });
 }
 
--- sstd_qt_qml_quick_library_application/private_application.hpp
+++ sstd_qt_qml_quick_library_application/private_application.hpp
@@ -3,6 +3,7 @@
 #include <sstd_library.hpp>
 #include <QtCore/qobject.h>
 #include <QtQml/qqmlcontext.h>
+#include <QtGui/qcolor.h>
 
 namespace sstd {
     namespace global {
@@ -12,42 +13,40 @@ namespace sstd {
         private:
             Q_PROPERTY(int version READ getVersion FINAL)
         private:
-            
+            Q_PROPERTY(QColor accent READ getAccent NOTIFY accentChanged)
+        private:
+            Q_PROPERTY(QColor background READ getBackground NOTIFY backgroundChanged)
+        private:
+            Q_PROPERTY(QColor foreground READ getForeground NOTIFY foregroundChanged)
+        private:
+            Q_PROPERTY(QColor primary READ getPrimary NOTIFY primaryChanged)
+        private:
+            Q_PROPERTY(bool isDark READ isDark WRITE setIsDark NOTIFY isDarkChanged)
         public:
             StaticGlobal();
             ~StaticGlobal();
         public:
             int getVersion() const;
+        public:
+            void setIsDark(bool);
+            Q_SIGNAL void isDarkChanged();
+        public:
+            int    getElevation() const;
+            QColor getAccent() const;
+            QColor getBackground() const;
+            QColor getForeground() const;
+            QColor getPrimary() const;
+            bool isDark() const;
+        public:
+            Q_SIGNAL void accentChanged();
+            Q_SIGNAL void backgroundChanged();
+            Q_SIGNAL void foregroundChanged();
+            Q_SIGNAL void primaryChanged();
+        private:
+            bool thisIsDark{ false };
         private:
             sstd_class(StaticGlobal);
         };
-
-        class Static : public QObject {
-            Q_OBJECT
-        private:
-            Q_PROPERTY(StaticGlobal* sharedObject READ getSharedObject)
-        private:
-            Q_PROPERTY(QObject* appStyle READ getAppStyle)
-        public:
-            inline StaticGlobal *getSharedObject() const;
-            inline QObject * getAppStyle() const;
-        public:
-            Static(StaticGlobal *,QObject *);
-            ~Static();
-        private:
-            StaticGlobal * const thisSharedObject;
-            QObject * const thisAppStyle;
-        public:
-            sstd_class(Static);
-        };
-
-        inline StaticGlobal *Static::getSharedObject() const {
-            return thisSharedObject;
-        }
-
-        inline QObject * Static::getAppStyle() const {
-            return thisAppStyle;
-        }
 
     }/*namespace global*/
 }/*namespace sstd*/
