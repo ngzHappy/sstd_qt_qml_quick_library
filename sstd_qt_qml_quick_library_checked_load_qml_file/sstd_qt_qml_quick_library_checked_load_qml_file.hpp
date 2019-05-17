8e3fe357fb61c13a709248bb2366d70e85a64514
ngzHappy <zhaixueqiang@hotmail.com>
2019-05-17 09:15:06 +0800

    Update sstd_qt_qml_quick_library_checked_load_qml_file.hpp

--- sstd_qt_qml_quick_library_checked_load_qml_file/sstd_qt_qml_quick_library_checked_load_qml_file.hpp
+++ sstd_qt_qml_quick_library_checked_load_qml_file/sstd_qt_qml_quick_library_checked_load_qml_file.hpp
@@ -1,6 +1,6 @@
-namespace sstd{
+namespace sstd {
 
-    namespace _private_sstd_checked_load_qml_file{
+    namespace _private_sstd_checked_load_qml_file {
 
         class ConnectData {
         public:
@@ -15,22 +15,24 @@ namespace sstd{
     }/*namespace _private_sstd_checked_load_qml_file*/
 
     inline void checkedLoadQmlFile(QQmlApplicationEngine * argEngine,
-                                   QCoreApplication * argApp,
-                                   const QUrl & argUrl){
+        QCoreApplication * argApp,
+        const QUrl & argUrl) {
         auto varConnectData = sstd_make_shared< _private_sstd_checked_load_qml_file::ConnectData >(
             argApp,
             argEngine,
             argUrl);
-        varConnectData ->connectIndex =
-                argEngine->connect( argEngine,&QQmlApplicationEngine::objectCreated,
-                                    argApp , [varConnectData](QObject *argAnsObj, const QUrl &argAnsUrl){
-                if(argAnsUrl == varConnectData->url){
+        varConnectData->connectIndex =
+            argEngine->connect(argEngine, &QQmlApplicationEngine::objectCreated,
+                argApp, [varConnectData](QObject *argAnsObj, const QUrl &argAnsUrl) {
+            if (argAnsUrl == varConnectData->url) {
                 varConnectData->engine->disconnect(varConnectData->connectIndex);
-    }else{return;}
-                if( !argAnsObj ){
+            } else {
+                return;
+            }
+            if (!argAnsObj) {
                 varConnectData->app->exit(-1);
-    }
-    });
+            }
+        });
         argEngine->load(argUrl);
     }
 
