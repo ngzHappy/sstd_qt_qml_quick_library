2d22fca7e33cdad4e3452cdd97c0c5a8482980fc
ngzHappy <zhaixueqiang@hotmail.com>
2019-07-09 21:38:08 +0800

    +update

--- sstd_qt_qml_quick_library_dynamic_property/sstd_qt_qml_quick_library_dynamic_property.cpp
+++ sstd_qt_qml_quick_library_dynamic_property/sstd_qt_qml_quick_library_dynamic_property.cpp
@@ -1,46 +1,46 @@
 ﻿#include "sstd_qt_qml_quick_library_dynamic_property.hpp"
 
-namespace sstd{
+namespace sstd {
 
 #ifndef Q_MOC_RUN
     using Mutex = \uacae_dynamic_property_detail::Mutex;
 #endif
 
     inline uint theObjectID() {
-        static const uint varAns = []() ->uint{
-                return QObject::registerUserData();
-    }();
+        static const uint varAns = []() ->uint {
+            return QObject::registerUserData();
+        }();
         return varAns;
     }
 
     /*必须是2的整数倍*/
-    inline constexpr int theMutexSize(){
+    inline constexpr int theMutexSize() {
         return 256;
     }
 
-    inline Mutex * theMutex(){
+    inline Mutex * theMutex() {
         using T = std::array< Mutex, theMutexSize()>;
         auto static globalAns = sstd_new<T>();
         return globalAns->data();
     }
 
-    inline Mutex * theMutex(const void * arg){
-        auto varPointer = reinterpret_cast< std::uintptr_t >(arg);
-        return theMutex()+((varPointer>>1)&static_cast<std::uintptr_t>(theMutexSize()-1));
+    inline Mutex * theMutex(const void * arg) {
+        auto varPointer = reinterpret_cast<std::uintptr_t>(arg);
+        return theMutex() + ((varPointer >> 1)&static_cast<std::uintptr_t>(theMutexSize() - 1));
     }
 
 #ifndef Q_MOC_RUN
     namespace \uacae_dynamic_property_detail{
 
-        SSTD_QT_SYMBOL_DECL Mutex * mutex(){
+        SSTD_QT_SYMBOL_DECL Mutex * mutex() {
             return theMutex();
         }
 
-        SSTD_QT_SYMBOL_DECL int mutex_size(){
+        SSTD_QT_SYMBOL_DECL int mutex_size() {
             return theMutexSize();
         }
 
-        SSTD_QT_SYMBOL_DECL uint object_id(){
+        SSTD_QT_SYMBOL_DECL uint object_id() {
             return theObjectID();
         }
 
@@ -48,71 +48,71 @@ namespace sstd{
 #endif
 
 
-    SSTD_QT_SYMBOL_DECL DynamicPropertyMap * getDynamicPropertyMap(QObject * arg){
+        SSTD_QT_SYMBOL_DECL DynamicPropertyMap * getDynamicPropertyMap(QObject * arg) {
 
-        if( arg == nullptr ){
+        if (arg == nullptr) {
             return {};
         }
 
         auto varMutex = theMutex(arg);
-        const auto varID = theObjectID() ;
+        const auto varID = theObjectID();
 
         {
-            std::shared_lock varReadLock{*varMutex};
-            auto varUserData = static_cast<DynamicPropertyMap *>( arg->userData( varID ) );
-            if(varUserData){
-                return varUserData ;
+            std::shared_lock varReadLock{ *varMutex };
+            auto varUserData = static_cast<DynamicPropertyMap *>(arg->userData(varID));
+            if (varUserData) {
+                return varUserData;
             }
         }
 
-        std::unique_lock varWriteLock{*varMutex};
+        std::unique_lock varWriteLock{ *varMutex };
         {
-            auto varUserData = static_cast<DynamicPropertyMap *>( arg->userData( varID ) );
-            if(varUserData){
+            auto varUserData = static_cast<DynamicPropertyMap *>(arg->userData(varID));
+            if (varUserData) {
                 return varUserData;
             }
         }
 
         auto varProperty = sstd_new<DynamicPropertyMap>();
-        arg->setUserData( varID, varProperty );
+        arg->setUserData(varID, varProperty);
         return varProperty;
 
     }
 
-    bool DynamicPropertyMap::has(const DynamicPropertyMapKey & arg) const{
+    bool DynamicPropertyMap::has(const DynamicPropertyMapKey & arg) const {
         std::shared_lock varRead{ thisMutex };
         auto varPos = thisMap.find(arg);
-        return varPos!=thisMap.end();
+        return varPos != thisMap.end();
     }
 
-    DynamicPropertyMap::ThisQObjectUserDataPointer DynamicPropertyMap::get(const DynamicPropertyMapKey & arg) const{
+    DynamicPropertyMap::ThisQObjectUserDataPointer DynamicPropertyMap::get(const DynamicPropertyMapKey & arg) const {
         std::shared_lock varRead{ thisMutex };
         auto varPos = thisMap.find(arg);
-        if(varPos == thisMap.end()){
+        if (varPos == thisMap.end()) {
             return {};
         }
         return varPos->second.get();
     }
 
-    void DynamicPropertyMap::erase(const DynamicPropertyMapKey & arg){
+    void DynamicPropertyMap::erase(const DynamicPropertyMapKey & arg) {
         std::unique_lock varWrite{ thisMutex };
         thisMap.erase(arg);
     }
 
-    void DynamicPropertyMap::put(const DynamicPropertyMapKey & arg, ThisQObjectUserDataPointer argV){
+    void DynamicPropertyMap::put(const DynamicPropertyMapKey & arg, ThisQObjectUserDataPointer argV) {
         std::unique_lock varWrite{ thisMutex };
-        thisMap[arg]=OwnThisQObjectUserDataPointer(argV);
+        thisMap[arg] = OwnThisQObjectUserDataPointer(argV);
     }
 
-    DynamicPropertyMap::DynamicPropertyMap(){
+    DynamicPropertyMap::DynamicPropertyMap() {
     }
 
-    DynamicPropertyMap::~DynamicPropertyMap(){
+    DynamicPropertyMap::~DynamicPropertyMap() {
         std::unique_lock varWrite{ thisMutex };
         thisMap.clear();
     }
 
-    SSTD_QT_SYMBOL_DECL std::intptr_t registerDynamicPropertyMapKey(){
+    SSTD_QT_SYMBOL_DECL std::intptr_t registerDynamicPropertyMapKey() {
         static std::atomic< std::intptr_t > globalAns;
         return ++globalAns;
     }
--- sstd_qt_qml_quick_library_dynamic_property/sstd_qt_qml_quick_library_dynamic_property.hpp
+++ sstd_qt_qml_quick_library_dynamic_property/sstd_qt_qml_quick_library_dynamic_property.hpp
@@ -5,7 +5,7 @@
 #include <QtCore/qobject.h>
 #include <map>
 
-namespace sstd{
+namespace sstd {
 
 #ifndef Q_MOC_RUN
     namespace \uacae_dynamic_property_detail{
@@ -21,26 +21,26 @@ namespace sstd{
 #endif
 
 #ifndef Q_MOC_RUN
-    using DynamicPropertyMapKeyDetail =
-    std::variant< std::intptr_t ,
-    QString ,
-    \uacae_dynamic_property_detail::KnowDynamicPropertyMapKey>;
+        using DynamicPropertyMapKeyDetail =
+        std::variant< std::intptr_t,
+        QString,
+        \uacae_dynamic_property_detail::KnowDynamicPropertyMapKey>;
 #endif
 
     SSTD_QT_SYMBOL_DECL std::intptr_t registerDynamicPropertyMapKey();
 
-    class DynamicPropertyMapKey :
-            public DynamicPropertyMapKeyDetail {
+    class DynamicPropertyMapKey final :
+        public DynamicPropertyMapKeyDetail {
         using Super = DynamicPropertyMapKeyDetail;
     public:
-        inline DynamicPropertyMapKey(){
+        inline DynamicPropertyMapKey() {
         }
-        inline DynamicPropertyMapKey(std::intptr_t arg) : Super(arg){
+        inline DynamicPropertyMapKey(std::intptr_t arg) : Super(arg) {
         }
-        inline DynamicPropertyMapKey(QString arg) : Super(std::move(arg)){
+        inline DynamicPropertyMapKey(QString arg) : Super(std::move(arg)) {
         }
 #ifndef Q_MOC_RUN
-        inline DynamicPropertyMapKey(\uacae_dynamic_property_detail::KnowDynamicPropertyMapKey arg):Super(arg){
+        inline DynamicPropertyMapKey(\uacae_dynamic_property_detail::KnowDynamicPropertyMapKey arg) : Super(arg) {
         }
 #endif
     private:
@@ -51,50 +51,50 @@ namespace sstd{
     SSTD_QT_SYMBOL_DECL DynamicPropertyMap * getDynamicPropertyMap(QObject *);
 
     class SSTD_QT_SYMBOL_DECL DynamicPropertyMap final :
-            public QObjectUserData {
-        using ThisQObjectUserDataPointer = QObjectUserData *;
-        using OwnThisQObjectUserDataPointer = std::unique_ptr<QObjectUserData>;
-    public:
-        bool has(const DynamicPropertyMapKey &) const;
-        void erase(const DynamicPropertyMapKey &);
-        ThisQObjectUserDataPointer get(const DynamicPropertyMapKey &) const;
-        void put(const DynamicPropertyMapKey &,ThisQObjectUserDataPointer);
-        template<typename T,typename ... Args >
-        inline std::pair<T *,bool> construct(const DynamicPropertyMapKey &,Args && ...);
-        DynamicPropertyMap();
-        ~DynamicPropertyMap();
-    private:
-        friend DynamicPropertyMap * getDynamicPropertyMap(QObject *);
-        mutable std::shared_mutex thisMutex;
-        using Map = std::map< DynamicPropertyMapKey,
-        OwnThisQObjectUserDataPointer,
-        std::less<>,
-        sstd::allocator< std::pair<const DynamicPropertyMapKey,OwnThisQObjectUserDataPointer > > >;
-        Map thisMap;
-    private:
-        sstd_class(DynamicPropertyMap);
+    public QObjectUserData{
+using ThisQObjectUserDataPointer = QObjectUserData * ;
+using OwnThisQObjectUserDataPointer = std::unique_ptr<QObjectUserData>;
+public:
+    bool has(const DynamicPropertyMapKey &) const;
+    void erase(const DynamicPropertyMapKey &);
+    ThisQObjectUserDataPointer get(const DynamicPropertyMapKey &) const;
+    void put(const DynamicPropertyMapKey &,ThisQObjectUserDataPointer);
+    template<typename T,typename ... Args >
+    inline std::pair<T *,bool> construct(const DynamicPropertyMapKey &,Args && ...);
+    DynamicPropertyMap();
+    ~DynamicPropertyMap();
+private:
+    friend DynamicPropertyMap * getDynamicPropertyMap(QObject *);
+    mutable std::shared_mutex thisMutex;
+    using Map = std::map< DynamicPropertyMapKey,
+    OwnThisQObjectUserDataPointer,
+    std::less<>,
+    sstd::allocator< std::pair<const DynamicPropertyMapKey,OwnThisQObjectUserDataPointer > > >;
+    Map thisMap;
+private:
+    sstd_class(DynamicPropertyMap);
     };
 
-    template<typename T,typename ... Args>
-    inline std::pair<T *,bool> DynamicPropertyMap::construct(
-            const DynamicPropertyMapKey & arg,Args && ...args){
-        {
-            std::shared_lock varRead{ thisMutex };
-            auto varPos = thisMap.find(arg);
-            if(varPos!=thisMap.end()){
-                return {static_cast<T *>( varPos->second.get() ),false};
+    template<typename T, typename ... Args>
+    inline std::pair<T *, bool> DynamicPropertyMap::construct(
+        const DynamicPropertyMapKey & arg, Args && ...args) {
+            {
+                std::shared_lock varRead{ thisMutex };
+                auto varPos = thisMap.find(arg);
+                if (varPos != thisMap.end()) {
+                    return { static_cast<T *>(varPos->second.get()),false };
+                }
             }
-        }
-        std::unique_lock varWrite{ thisMutex };
-        {
-            auto varPos = thisMap.find(arg);
-            if(varPos!=thisMap.end()){
-                return {static_cast<T *>( varPos->second.get() ),false};
+            std::unique_lock varWrite{ thisMutex };
+            {
+                auto varPos = thisMap.find(arg);
+                if (varPos != thisMap.end()) {
+                    return { static_cast<T *>(varPos->second.get()),false };
+                }
             }
-        }
-        auto varAns = sstd_new<T>(std::forward<Args>(args)...);
-        thisMap[arg]=OwnThisQObjectUserDataPointer(varAns);
-        return {varAns,true};
+            auto varAns = sstd_new<T>(std::forward<Args>(args)...);
+            thisMap[arg] = OwnThisQObjectUserDataPointer(varAns);
+            return { varAns,true };
     }
 
 }/*namespace sstd*/
