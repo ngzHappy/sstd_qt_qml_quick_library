d4e246cd3b9ab9fdf958de32602e65c97603c42d
ngzHappy <zhaixueqiang@hotmail.com>
2019-07-19 18:07:13 +0800

    Update sstd_qt_qml_quick_library_cosmetic_pen.hpp

--- sstd_qt_qml_quick_library_cosmetic_pen.hpp
+++ sstd_qt_qml_quick_library_cosmetic_pen.hpp
@@ -4,48 +4,18 @@
 #include <QtGui/qpen.h>
 
 namespace sstd {
-    class CosmeticPen : public QPen {
-        inline void implConstruct() {
-            this->setCosmetic(true);
-        }
-        template<typename T>
-        using rcvr_t = std::remove_cv_t<std::remove_reference_t<T>>;
-        template<typename T>
-        using is_this = std::is_same<CosmeticPen, rcvr_t<T>>;
-        template<typename T>
-        inline static constexpr bool is_not_this() {
-            return !is_this<T>::value;
-        }
-    public:
-        inline CosmeticPen() {
-            this->implConstruct();
-        }
-        inline CosmeticPen(const CosmeticPen & arg) :
-            QPen{ arg } {
-            this->implConstruct();
-        }
-        inline CosmeticPen(CosmeticPen && arg) :
-            QPen{ std::move(arg) } {
-            this->implConstruct();
-        }
-        template<typename T0, typename T1, typename ... Args>
-        inline CosmeticPen(T0&&arg0, T1&&arg1, Args&&...args) :
-            QPen(std::forward<T0>(arg0),
-                std::forward<T1>(arg1),
-                std::forward<Args>(args)...) {
-            this->implConstruct();
-        }
-        template<typename T0,
-            typename = std::enable_if_t<is_not_this<T0>()>>
-            inline CosmeticPen(T0&&arg0) :
-            QPen(std::forward<T0>(arg0)) {
-            this->implConstruct();
-        }
-        CosmeticPen & operator=(const CosmeticPen &) = default;
-        CosmeticPen & operator=(CosmeticPen &&) = default;
-    private:
-        sstd_class(CosmeticPen);
-    };
+
+    namespace the {
+        class SetCosmeticPen {
+        public:
+            inline void operator()(QPen * arg) const {
+                arg->setCosmetic(true);
+            }
+        };
+    }/*namespace the*/
+
+    using CosmeticPen = ElementAt<QPen, the::SetCosmeticPen>;
+
 }/*namespace sstd*/
 
 
